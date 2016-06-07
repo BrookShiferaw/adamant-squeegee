f264e9ea486dcf00ae60a5e42a87c442f39107f4
Brook Shiferaw <brook.tight@gmail.com>
2016-06-07 11:23:07 +1000

    latest update

---
+++ VWM_pilot/pilot_data/error/untitled1.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 18 18:40:58 2015
+
+@author: Eyetracking
+"""
+
+runfile('C:/Users/Eyetracking/Documents/adamant-squeegee/Brook/VWM_task/VWMtask_a.py', wdir='C:/Users/Eyetracking/Documents/adamant-squeegee/Brook/VWM_task')
+FreeType import Failed: Freetype library not found
+1.9387  WARNING         Movie2 stim could not be imported and won't be available
+2.5323  WARNING         TextBox stim could not be imported and won't be available.
+2.5323  WARNING         TextBox Font Manager Found No Fonts.
+4.1279  WARNING         psychopy.parallel has been imported but no parallel port driver found. Install either input32 or dlportio
+254.8715        ERROR   Error in framebuffer activation
+254.8715        WARNING         Framebuffer object (FBO) not supported on this graphics card
+255.0071        WARNING         t of last frame was 5.33ms (=1/187)
+255.0217        WARNING         t of last frame was 5.44ms (=1/183)
+255.0393        WARNING         t of last frame was 5.40ms (=1/185)
+To exit: use 'exit', 'quit', or Ctrl-D.
+An exception has occurred, use %tb to see the full traceback.
+
+SystemExit: 0
+
+
+runfile('C:/Users/Eyetracking/Documents/adamant-squeegee/Brook/VWM_task/VWMtask_a.py', wdir='C:/Users/Eyetracking/Documents/adamant-squeegee/Brook/VWM_task')
+saved data to u'C:\\Users\\Eyetracking\\Documents\\adamant-squeegee\\Brook\\VWM_task\\data/04a_test1_2015_Dec_18_1605.csv'
+3838.2372       ERROR   Error in framebuffer activation
+3838.2372       WARNING         Framebuffer object (FBO) not supported on this graphics card
+3838.3662       WARNING         t of last frame was 5.50ms (=1/181)
+3838.3837       WARNING         t of last frame was 5.46ms (=1/183)
+To exit: use 'exit', 'quit', or Ctrl-D.
+An exception has occurred, use %tb to see the full traceback.
+
+SystemExit: 0
+
+
